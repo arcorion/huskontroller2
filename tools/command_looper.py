0928264e94573afc32ed95523b8990cfce6df20d
Christopher Lee Crader <67856679+arcorion@users.noreply.github.com>
2025-04-28 16:26:08 -0700

    Add command looper tool (needs work)

---
+++ tools/command_looper.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import serial
+import select
+from serial.tools import list_ports
+
+def main():
+    cycle_read_write(attach_device())
+
+def attach_device():
+
+    port_list = list_ports.comports()
+    port_names = []
+    for port in port_list:
+        port_names += port
+    device = serial.Serial(port_names[0], 9600)
+    return device
+
+def cycle_read_write(device):
+    command = ""
+    timeout = 5
+
+    while (command.lower() != 'quit'):
+        command = input('Command? ("quit to quit") ')
+        device.write(command.encode())
+        read, _, _ = select.select([device], [], [], timeout)
+        device_output = device.read(0x100)
+
+        print(device_output)
+
+if __name__ == '__main__':
+    main()
