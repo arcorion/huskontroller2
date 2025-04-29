ceb88406568e35dd8ee800f421e6065dc74dcc10
Christopher Lee Crader <67856679+arcorion@users.noreply.github.com>
2025-04-28 18:19:45 -0700

    Added outline to DeviceInterface class

--- device_interface.py
+++ device_interface.py
@@ -1,2 +1,17 @@
 class DeviceInterface():
-    pass
+    """
+    The DeviceInterface class is meant to represent a generic device.
+    It should function as a template for other classes implementing it.
+    
+    The DeviceInterface object will interact with the controller by passing
+    along commands requested of it and by updating the controller with any
+    pertinent changes to its state.
+    """
+    def __init__():
+        pass
+    
+    def update_controller():
+        pass
+
+    def send_command():
+        pass
