6ed6e16594e6b24044f2754db3e61c69ede8d6b2
Christopher Lee Crader <67856679+arcorion@users.noreply.github.com>
2025-04-28 18:12:20 -0700

    Add comments to command_looper.py

--- tools/command_looper.py
+++ tools/command_looper.py
@@ -7,6 +7,9 @@ def main():
     cycle_read_write(attach_device())
 
 def attach_device():
+    """
+    Attaches the first serial device found by the computer.
+    """
     port_list = list_ports.comports()
     port_names = []
     for port in port_list:
@@ -15,10 +18,17 @@ def attach_device():
     return device
 
 def cycle_read_write(device):
+    """
+    Takes a command and writes it to the serial device. Prints out the result from said device.
+    """
     command = ""
     timeout = 5
 
     while (command.lower() != 'quit'):
+        # Taken from here - https://stackoverflow.com/questions/26047544/python-serial-port-listener
+        # Still need to read up on serial.Serial.read
+        # https://pyserial.readthedocs.io/en/latest/pyserial_api.html?highlight=serial%20read#serial.Serial.read
+
         command = input('Command? ("quit to quit") ')
         device.write(command.encode())
         read, _, _ = select.select([device], [], [], timeout)
