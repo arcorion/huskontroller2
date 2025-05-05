4f4c4e699a0f75372eab05a99f4fd32abe00d795
Christopher Lee Crader <67856679+arcorion@users.noreply.github.com>
2025-05-05 15:23:49 -0700

    Test removal of string strip

--- tools/command_looper.py
+++ tools/command_looper.py
@@ -36,7 +36,7 @@ def cycle_read(device):
     while True:
         read, _, _ = select.select([device], [], [])
         device_output = device.read_until("\r\n")
-        device_output = device_output.decode().strip("\r\n")
+        device_output = device_output.decode() #.strip("\r\n")
         if device_output != "":
             print(": " + device_output)
 
