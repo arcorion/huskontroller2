86457d88b8ef673e4995643d37e8585381040fd9
Christopher Lee Crader <67856679+arcorion@users.noreply.github.com>
2025-05-05 16:06:01 -0700

    Add content collection for device output.

--- tools/command_looper.py
+++ tools/command_looper.py
@@ -32,14 +32,18 @@ def cycle_read(device):
     """
     print("Press CTRL+C to quit")
     timeout = 10
+    device_content = ""
 
     while True:
         read, _, _ = select.select([device], [], [])
         device_output = device.read_until("\r\n")
-        print(device_output.decode())
         device_output = device_output.decode().strip("\r\n")
         if len(device_output) != 0:
-            print(f': {device_output}', end='')
+            device_content = device_content + device_output
+        elif len(device_output) == 0:
+            print(f': {device_content}')
+        else:
+            print("Error!")
 
 def cycle_read_write(device):
     """
