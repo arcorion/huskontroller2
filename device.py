253a5cd7246899cac8f658af0d737023fbb23889
Christopher Lee Crader <67856679+arcorion@users.noreply.github.com>
2025-05-06 10:19:41 -0700

    Rename DeviceInterface to Device

--- device.py
+++ device.py
@@ -1,9 +1,9 @@
-class DeviceInterface:
+class Device:
     """
-    The DeviceInterface class is meant to represent a generic device.
+    The Device class is meant to represent a generic device.
     It should function as a template for other classes implementing it.
     
-    The DeviceInterface object will interact with the controller by passing
+    The Device object will interact with the controller by passing
     along commands requested of it and by updating the controller with any
     pertinent changes to its state.
     """
