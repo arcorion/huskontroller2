9ff8eb958a3322c720f104dfb58aafe8868f6fab
Christopher Lee Crader <67856679+arcorion@users.noreply.github.com>
2025-05-05 17:11:37 -0700

    Remove test statement.

--- tools/command_looper.py
+++ tools/command_looper.py
@@ -38,7 +38,6 @@ def cycle_read(device):
         read, _, _ = select.select([device], [], [])
         device_output = device.read_until("\r\n")
         device_output = device_output.decode().strip("\r\n")
-        print("test statement")
         if len(device_output) != 0:
             device_content = device_content + device_output
         elif len(device_output) == 0:
