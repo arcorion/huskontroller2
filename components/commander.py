88e43f7daf4b42d725952857c004b5d3668a1fe3
Christopher Lee Crader <67856679+arcorion@users.noreply.github.com>
2025-05-28 08:58:50 -0700

    Change command module to commander.

---
+++ components/commander.py
@@ -0,0 +1,59 @@
+import serial
+from serial.tools import list_ports
+
+class Commander:
+    """
+    Commander handles the command string that is
+    sent to the Extron device.
+
+    It associates the commands themselves with a useful
+    name for easy use in other methods.
+    """
+    def __init__(self, device):
+        self._device = device
+
+        self.command_list = {
+            'select_podium': '1!',
+            'select_hdmi': '2!',
+            'select_usbc': '3!',
+            'select_vga': '4!',
+            'view_current_input': '!',
+            'enable_freeze': '1*1F',
+            'disable_freeze': '1*0F',
+            'get_freeze_status': '1F',
+            'disable_video': '1*1B',
+            'enable_video': '1*0B',
+            'get_video_status': '1*B',
+            'enable_projector': 'W+snds9*9|%02PON%03',
+            'disable_projector': 'W+snds9*9|%02POF%03',
+            'get_volume': 'V',
+            'disable_audio': '1Z',
+            'enable_audio': '0Z',
+            'get_audio_status': 'Z'
+        }
+
+        try:
+            port_list = list_ports.comports()
+            port_names = []
+            for port in port_list:
+                port_names += port
+            self.extron_device = serial.Serial(port_names[0], 9600)
+            self.logger.info(f'Device {self.extron_device} created.')
+        except serial.SerialException:
+            self.logger.error(f'Error opening connection to port: {port_list[0]}')
+
+    def send_command(self, command, custom=False):
+        """
+        Takes a command string and sends the command as an
+        encoded byte string to the Extron device.
+        """
+        command_string = self.command_list.get(command)
+        if command_string:
+            self._device.write(command_string.encode())
+            self.logger.info(f'Command sent: {command_string}')
+        elif custom == True:
+            self._device.write(command.encode())
+            self.logger.info(f'Custom command sent: {command}')
+        else:
+            self.logger.error(f'Unsupported command: {command}')
+
